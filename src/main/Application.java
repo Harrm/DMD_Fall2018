8dd65f0d8d646d19b0c7106ee5f781c6a20cda0b
iambackend <iambackend@gmail.com>
2018-11-21 20:44:34 +0300

    Test connection with database and untested first SELECT.

--- src/main/Application.java
+++ src/main/Application.java
@@ -1,7 +1,34 @@
 package main;
 
+import org.postgresql.Driver;
+
+import java.sql.*;
+
 public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hi, Maven!");
+    public static void main(String[] args) throws SQLException {
+        Connection c = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            Driver driver = new Driver();
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://elmer.db.elephantsql.com:5432/lherrbcv",
+                            "lherrbcv", "password");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            System.exit(0);
+        }
+        System.out.println("Opened database successfully");
+        Statement s = c.createStatement();
+        String sql = "SELECT *\n" +
+                "FROM taxi_service.customer as c\n" +
+                "       JOIN taxi_service.rents as r ON c.username = 'INSERT' AND r.customer = c.username\n" +
+                "       JOIN taxi_service.car ON car.license_plate LIKE 'AH%' AND car.license_plate = r.car;";
+        ResultSet rs = s.executeQuery(sql);
+        while (rs.next()) {
+            System.out.println(rs.getString("name"));
+        }
+        s.close();
+        c.close();
     }
 }
