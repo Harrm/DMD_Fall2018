cf7c445c2c50548257a59226dd492167bc780591
iambackend <iambackend@gmail.com>
2018-11-23 12:07:27 +0300

    Test as separate function and password.txt in gitignore.

--- src/main/Application.java
+++ src/main/Application.java
@@ -1,18 +1,26 @@
 package main;
 
-import org.postgresql.Driver;
-
 import java.io.File;
 import java.sql.*;
 import java.util.Scanner;
 
 public class Application {
+
+    private static Connection c = null;
+
     public static void main(String[] args) throws SQLException {
-        Connection c = null;
         try {
             Class.forName("org.postgresql.Driver");
-            Scanner passwordScanner = new Scanner(new File("password.txt"));
-            String password = passwordScanner.nextLine();
+            String password = null;
+            try {
+                Scanner passwordScanner = new Scanner(new File("password.txt"));
+                password = passwordScanner.nextLine();
+                if (password == null) throw new Exception("add password.txt file with password");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                System.exit(0);
+            }
             c = DriverManager
                     .getConnection("jdbc:postgresql://elmer.db.elephantsql.com:5432/lherrbcv",
                             "lherrbcv", password);
@@ -22,16 +30,17 @@ public class Application {
             System.exit(0);
         }
         System.out.println("Opened database successfully");
+        test1("pidor");
+        c.close();
+    }
+
+    private static ResultSet test1(String username) throws SQLException {
         Statement s = c.createStatement();
         String sql = "SELECT *\n" +
                 "FROM taxi_service.customer as c\n" +
-                "       JOIN taxi_service.rents as r ON c.username = 'INSERT' AND r.customer = c.username\n" +
-                "       JOIN taxi_service.car ON car.license_plate LIKE 'AH%' AND car.license_plate = r.car;";
-        ResultSet rs = s.executeQuery(sql);
-        while (rs.next()) {
-            System.out.println(rs.getString("name"));
-        }
-        s.close();
-        c.close();
+                "       JOIN taxi_service.rents as r ON c.username = '" + username + "' AND r.customer = c.username\n" +
+                "       JOIN taxi_service.car ON car.license_plate LIKE 'AN%' AND car.license_plate = r.car AND car.color = 'red';";
+        return s.executeQuery(sql);
     }
+
 }
