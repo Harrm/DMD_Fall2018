d40cd09e6ed81e417326f3069b9ddc7eb59e43f1
iambackend <iambackend@gmail.com>
2018-11-24 13:25:36 +0300

    finish Queries interface

--- src/main/Queries.java
+++ src/main/Queries.java
@@ -8,4 +8,11 @@ public interface Queries {
     public ResultSet query1(String username) throws SQLException;
 //    public ResultSet query2(LocalDate date);
     public ResultSet query3() throws SQLException;
+//    public ResultSet query4(String username) throws SQLException;
+//    public ResultSet query5(LocalDate date) throws SQLException;
+//    public ResultSet query6() throws SQLException;
+//    public ResultSet query7() throws SQLException;
+//    public ResultSet query8() throws SQLException;
+//    public ResultSet query9() throws SQLException;
+//    public ResultSet query10() throws SQLException;
 }
